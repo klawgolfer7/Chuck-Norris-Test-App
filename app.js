67229e76d7db2b9e54c9b882fc40899245a70dbd
klawgolfer7 <kevinrlawler@gmail.com>
2017-08-29 14:07:19 -0400

    adding ajax GET and button in jokes.html

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 var userInput = $("#searchInput").val;
 
 console.log(userInput)
+
+$("button").on("click", function() {
+	var search = $(this).data("search");
+
+	var queryURL = "https://api.chucknorris.io/jokes/search?query={" + "search" + "}";
+
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	})
+
+	
+})
